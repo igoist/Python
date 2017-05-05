3870a4cc18d420c9a439fbffdf305ee57d6c451f
XuTianZhu <493512132@qq.com>
2017-05-05 20:01:27 +0800

    First commit & Compare to the old file, highlight the printed info

---
+++ psutil-proj/Integrated.py
@@ -0,0 +1,13 @@
+#-*-coding:utf-8-*-
+
+# import inspect
+# from cpu import cpu
+from mem import memory_print
+
+memory_print('m')
+
+# print (inspect.getsource(memory_print))
+
+# print '\033[0;37;40m'
+# print '*' * 50
+# print '\033[0m'
